fc95e122c56a1a59176630e62ed394615b210f99
Jakob Hellermann <jakob.hellermann@protonmail.com>
2023-10-30 11:30:32 +0100

    add phobos & deimos

--- cg/praktikum1/main.js
+++ cg/praktikum1/main.js
@@ -7,18 +7,24 @@ if (!BABYLON.Engine.isSupported()) {
 // -- PARAMETERS (frequency in per day, radius in AU) --
 // simulation parameters:
 let daysPerSecond = 1;
-let notToScaleMoonDistance = 120;
-let notToScaleBodySize = 3500;
+let notToScaleMoonDistance = 100;
+let notToScaleMarsMoonDistance = 1500;
+let notToScaleBodySize = 3000;
 let notToScaleSunSize = 1 / 200;
+let notToScaleMarsMoonSize = 50;
 
 // physically based:
 let distanceEarthMoon = 384_400;
 let distanceSunEarth = 150_000_000;
 let distanceSunMars = 228_000_000;
+let distanceMarsPhobos = 9_377;
+let distanceMarsDeimos = 23_460;
 let sunDiameter = 1_392_700;
 let earthDiameter = 12_742;
 let moonDiameter = 3_474.8;
 let marsDiameter = 6_779;
+let phobosDiameter = 22.2;
+let deimosDiameter = 12.6;
 
 let earthConfiguration = {
     revolutionFrequency: 1 / 365.24,
@@ -28,7 +34,7 @@ let earthConfiguration = {
 let moonConfiguration = {
     revolutionFrequency: 1 / 27.3,
     revolutionRadius: distanceEarthMoon / distanceSunEarth * notToScaleMoonDistance,
-    rotationFrequency: 1 / 27.3,
+    rotationFrequency: 1 / 27.3, // synchronous orbit
 };
 let satelliteConfiguration = {
     revolutionFrequency: 3 * moonConfiguration.revolutionFrequency,
@@ -39,6 +45,16 @@ let marsConfiguration = {
     revolutionFrequency: 1 / 687,
     revolutionRadius: distanceSunMars / distanceSunEarth,
     rotationFrequency: 1 / 1.616,
+};
+let phobosConfiguration = {
+    revolutionFrequency: 24 / 7.66,
+    revolutionRadius: distanceMarsPhobos / distanceSunEarth * notToScaleMarsMoonDistance,
+    rotationFrequency: 24 / 7.66 // synchronous orbit
+};
+let deimosConfiguration = {
+    revolutionFrequency: 24 / 30.31,
+    revolutionRadius: distanceMarsDeimos / distanceSunEarth * notToScaleMarsMoonDistance,
+    rotationFrequency: 24 / 30.31, // synchronous orbit
 };
 
 let sunRotationFrequency = 1 / 24.47;
@@ -52,7 +68,6 @@ const ambientLight = new BABYLON.HemisphericLight("HemiLight", new BABYLON.Vecto
 var pointLight = new BABYLON.PointLight("dir01", new BABYLON.Vector3(-0.0, -0.0, 0.0), scene);
 pointLight.diffuse = new BABYLON.Color3(1.0, 1.0, 1.0);
 
-
 addCoordinateAxis();
 addSkybox();
 
@@ -60,6 +75,8 @@ let sunSize = sunDiameter / distanceSunEarth * notToScaleBodySize * notToScaleSu
 let earthSize = earthDiameter / distanceSunEarth * notToScaleBodySize;
 let moonSize = moonDiameter / distanceSunEarth * notToScaleBodySize;
 let marsSize = marsDiameter / distanceSunEarth * notToScaleBodySize;
+let phobosSize = phobosDiameter / distanceSunEarth * notToScaleBodySize * notToScaleMarsMoonSize;
+let deimosSize = deimosDiameter / distanceSunEarth * notToScaleBodySize * notToScaleMarsMoonSize;
 let satelliteSize = 0.02;
 
 let earth = createBody(
@@ -73,6 +90,11 @@ let moon = createBody(
     createMaterial("default2", "assets/moon.jpg", new BABYLON.Color3(0, 0, 0), new BABYLON.Color3(0.2, 0.2, 0.2),
     )
 );
+let satellite = createBody(
+    "satellite",
+    satelliteSize,
+    createMaterial("default5", "assets/metal.jpg", new BABYLON.Color3(0.2, 0.2, 0.2), new BABYLON.Color3(0.3, 0.3, 0.3),)
+);
 let sun = createBody(
     "Sun",
     sunSize,
@@ -83,16 +105,23 @@ let mars = createBody(
     marsSize,
     createMaterial("default4", "assets/mars.jpg", new BABYLON.Color3(0, 0, 0), new BABYLON.Color3(0.2, 0.2, 0.2),)
 );
-let satellite = createBody(
-    "satellite",
-    satelliteSize,
-    createMaterial("default5", "assets/metal.jpg", new BABYLON.Color3(0.2, 0.2, 0.2), new BABYLON.Color3(0.3, 0.3, 0.3),)
+let phobos = createBody(
+    "phobos",
+    phobosSize,
+    // http://www.celestiamotherlode.net/catalog/marsmoons.html
+    createMaterial("default6", "assets/phobos.jpg", new BABYLON.Color3(0, 0, 0), new BABYLON.Color3(0.2, 0.2, 0.2),)
+);
+let deimos = createBody(
+    "deimos",
+    phobosSize,
+    createMaterial("default7", "assets/deimos.jpg", new BABYLON.Color3(0, 0, 0), new BABYLON.Color3(0.2, 0.2, 0.2),)
 );
 
 var startTime = new Date().getTime();
 var lastTime = startTime;
 
-let initialTimeOffset = Math.random() * 365;
+// let initialTimeOffset = Math.random() * 365;
+let initialTimeOffset = 200;
 
 function move(day, object, configuration, base = null) {
     object.position.x = configuration.revolutionRadius * Math.sin(2 * Math.PI * configuration.revolutionFrequency * day);
@@ -117,6 +146,8 @@ scene.registerBeforeRender(() => {
     move(day, moon, moonConfiguration, earth);
     move(day, satellite, satelliteConfiguration, moon);
     move(day, mars, marsConfiguration);
+    move(day, phobos, phobosConfiguration, mars);
+    move(day, deimos, deimosConfiguration, mars);
 
     sun.rotation.y = 2 * Math.PI * sunRotationFrequency * day;
 });
